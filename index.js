efe498efdb2a6782fd9f541012751c144856f7a5
Nayaker <nayak.primary@gmail.com>
2022-10-13 13:18:09 +0530

    added get/post codes for mongoDB

--- index.js
+++ index.js
@@ -1,71 +1,72 @@
 const express = require('express');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
+const mongoose = require('mongoose');
 
-// https://www.youtube.com/watch?v=pKd0Rpw7O48 - Watch this video 
-
-// https://www.youtube.com/watch?v=l8WPWK9mS5M
-
+app.use(express.json());
 
 
-app.use(express.json()); //middle ware - for reading the content from post body
 
-const data = [
-    {
-        name: "Atanu Nayak", 
-        age: "19",
-        company: "Nayak Enterprises",
-        dataAddress: "wzaa21k92Army82",
-    },
-    {
-        name: "Elon Musk", 
-        age: "51",
-        company: "Tesla Motors",
-        dataAddress: "wzaa21k92Army034",
-    }
-]
+const schema = new mongoose.Schema({ //---> basically just line datatype of a variable / structure
+    name: String,
+    age: Number, // -----> Here int is represented as Number
+    company: String,
+    dataId: String,
+});
+
+
+
+const Collection = new mongoose.model("Collection", schema);
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World, this is Atanu Nayak');
+    res.send('<div style="color:#333; font-size:16px;">You have successfully entered the <b>MongoDB world . . .</b><div>');
 });
 
-app.get('/data', (req, res) => {
-    res.send(data);
-});
-
-// app.get('/data/:dataNumber/:random', (req, res) => {
-//     res.send(req.params);
-// });
-
-app.get('/data/:dataNumber', (req, res) => {
-    res.send(data[req.params.dataNumber - 1]);
-});
-
-app.get('/data/findByDataAddress/:dataAddress', (req, res) => {
-    const req_data = data.find(c => c.dataAddress == req.params.dataAddress);
-
-    // req.params.dataAddress - By default is in string 
-
-    if(!req_data) res.status(404).send('No data is linked to the given dataAddress');
-
+app.post('/post', (req, res) => {
+    const { name, age, company } = req.body;
+    console.log(req.body);
     
-    res.send(req_data);
-});
+    const firstPostObject = new Collection({
+        name: name,
+        age: age, 
+        company: company,
+        dataId: uuidv4(),
+    })
 
-
-app.post('/data/add/', (req, res) => {
-    const id = uuidv4();
-
-    const postData = {
-        name : req.body.name, 
-        age : req.body.age,
-        company : req.body.company,
-        dataAddress : id,
-    };
-
-    data.push(postData);
-    res.send(data);
+    firstPostObject.save().then(() => {
+        res.json(req.body);
+    }).catch((err) => res.json({error : "Can't save to mongo!"}));
 })
 
+app.get('/get-data', (req, res) => {
+    Collection.find({},(err,result)=>{res.status(200).json(result.reverse())});
+})
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------ CONNECTION BELOW ------------------------------------------------
+
+
+
+const CONNECTION_URL = 'mongodb+srv://nobodycares:nobodycares@new1.7iwujpr.mongodb.net/?retryWrites=true&w=majority';
+
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.listen(port , ()=> {
+    console.log(`App Listening at http://localhost:${port}...`)
+    mongoose.connect(CONNECTION_URL,{
+        useNewUrlParser: true ,
+        useUnifiedTopology: true
+    }).then(()=>{
+        console.log('MongoDB Connection Successful !!!')
+    }).catch((err)=> console.log(err))
+  })
