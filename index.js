36ee3a3ea83f2ddda60b992f6085bd708e922b51
Atanu Nayak <93304796+Nayaker@users.noreply.github.com>
2023-01-04 02:15:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ app.get('/', (req, res) => {
     res.send('Hello World, our first API is running');
 });
 
+const allUsers = [
+    {
+        "username" : "zuck",
+        "name" : "Mark Zuckerburg",
+        "bio" : "I keep passwords as plain texts",
+    },
+    {
+        "username" : "esaverin",
+        "name" : "Eduardo Saverin",
+        "bio" : "Hello World!",
+    },
+]
+
+app.get('/user/:username', (req, res) => {
+  const username = req.params.username;
+  for(let i = 0; i<allUsers.length; i++){
+    if(allUsers[i].username === username){
+      res.send(allUsers[i]);
+      break;
+    }
+  }
+  res.send("No user found with given username");
+});
+
 const port = 8000;
 
-app.listen(port, () => console.log(`Listening on localhost:${port} ...`));
+app.listen(port, () => console.log(`Listening on http://localhost:${port} ...`));
